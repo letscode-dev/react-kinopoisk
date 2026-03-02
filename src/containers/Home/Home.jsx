9312e9292c7e9552963c9573d0ecefe249d9692d
Anton Sergeenkov <asergeenkov@combotech.net>
2026-03-02 21:26:37 +0400

    ok

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,7 +1,10 @@
+import Button from "@mui/material/Button";
+
 const Home = () => {
   return (
     <>
       <h1>Домашняя страница</h1>
+      <Button variant="contained">Text</Button>
     </>
   );
 };
